afbc32264507b7624c5df104d60417b98a72b92b
cmpereirad <89921052+cmpereirad@users.noreply.github.com>
2022-02-17 13:59:14 -0300

    video6

--- js/video.js
+++ js/video.js
@@ -1,16 +1,19 @@
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
+let video = document.getElementById("video")
 
-var x = document.getElementById("prueba");
-x.innerHTML = windowHeight;
-
-if( navigator.userAgent.match(/Android/i)
-|| navigator.userAgent.match(/webOS/i)
-|| navigator.userAgent.match(/iPhone/i)
-|| navigator.userAgent.match(/iPad/i)
-|| navigator.userAgent.match(/iPod/i)
-|| navigator.userAgent.match(/BlackBerry/i)
-|| navigator.userAgent.match(/Windows Phone/i)) {
-alert("You're using Mobile Device!!")}
-else {alert ("NO ES MOBILE")}
+if( windowWidth < 500 ) {
+    video.innerHTML += `
+        <video autoplay loop muted title="Dos santos Pereira">
+        <source src="servir_vino2.mp4" type="video/mp4">
+        </video>
+        `
+    }
+    else {console.log ("NO ES MOBILE")
+    video.innerHTML += `
+        <video autoplay loop muted title="Dos santos Pereira">
+        <source src="servir_vino1.mp4" type="video/mp4">
+        </video>
+        `   
+}
 ;
